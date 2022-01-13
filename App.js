fd90ae804068331c6c99d8a5a33c49a5e66d83ca
Amanda1024 <88503704+codewithamanda@users.noreply.github.com>
2022-01-13 09:57:22 -0800

    Delete expo placeholder images, Create WelcomeScreen and ViewImageScreen Componetes, Create colors.js to use as app wide color variables

--- App.js
+++ App.js
@@ -1,20 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, View} from 'react-native';
+import ViewImageScreen from './src/components/ViewImageScreen';
+import WelcomeScreen from './src/components/WelcomeScreen';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ViewImageScreen />
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+
 });
