e57e0f9256433039963d6eecf6b07120c3fbefd6
Amanda1024 <88503704+codewithamanda@users.noreply.github.com>
2022-01-16 14:47:47 -0800

    Create button components

---
+++ src/components/SecondaryButton.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Text, StyleSheet, Alert, TouchableOpacity } from 'react-native';
+
+import colors from '../config/colors';
+
+
+function SecondaryButton({title, onPress}) {
+    return (
+        <TouchableOpacity
+        style={styles.button} 
+        onPress={() => Alert.alert('Button pressed')}>
+            <Text style={styles.text}>
+            {title}</Text>
+        </TouchableOpacity>
+    );
+}
+
+const styles = StyleSheet.create({
+    button: {
+        backgroundColor: colors.secondary,
+        borderRadius: 50,
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 15,
+        width: '100%',
+    },
+    text: {
+        color: colors.white,
+        fontSize: 18,
+        textTransform: 'uppercase',
+        fontWeight: 'bold',
+    },
+});
+
+export default SecondaryButton;
